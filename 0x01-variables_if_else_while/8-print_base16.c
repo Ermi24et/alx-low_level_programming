6ef49e13441c4f1b3801c063ef0039023a36d7ba
Ermi24et <lilermi24@gmail.com>
2023-03-17 13:28:07 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+/**
+ * main - prints all numbers of base 16.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'a';
+	while (n <= 'f')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
+}
