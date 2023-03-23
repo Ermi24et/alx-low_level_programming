e38d4a26b9ccc0c81eb66ac5828368463c814726
Ermi24et <lilermi24@gmail.com>
2023-03-23 16:29:10 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c)
 
 int _isupper(int c);
@@ -19,3 +22,5 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void print_triangle(int size);
+
+#endif
