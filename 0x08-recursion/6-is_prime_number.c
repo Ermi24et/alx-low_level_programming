b85a9e0d2edcf163e7014d9f24daf9ed9f046597
Ermi24et <lilermi24@gmail.com>
2023-04-05 16:08:57 -0700

    checking prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * check - checks if a number is prime
- * @m - number
- * @n - number to check
- * Return: number
+ * @m: - int
+ * @n: - int
+ * Return: int
  */
 int check(int m, int n)
 {
 	if (n < 2 || n % m == 0)
 		return (0);
-	else if (m > n/2)
+	else if (m > n / 2)
 		return (1);
 	else
 		return (check(m + 1, n));
@@ -21,14 +21,13 @@ int check(int m, int n)
 
 /**
  * is_prime_number - checks if number is prime or not
- *@n the number to be checked
+ *@n: the number to be checked
  *Return: 1 if number is prime if not 0
  */
-
 int is_prime_number(int n)
 {
 	if (n == 2)
 		return (1);
-	return(check(2, n));
+	return (check(2, n));
 
 }
