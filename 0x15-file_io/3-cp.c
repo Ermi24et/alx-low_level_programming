6480c299dcbc299e9ace745ff6ccaf42845ba99f
Ermi24et <lilermi24@gmail.com>
2023-05-08 14:55:04 -0700

    copies contents of a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,19 +36,16 @@ int main(int argc, char *argv[])
 		m = read(from_f, b, 1024);
 		if (m == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
 			exit(98);
 		}
 		n = write(to_f, b, m);
 		if (n < m)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-
 	if (close(from_f) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_f), exit(100);
-
 	if (close(to_f) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_f), exit(100);
-
 	return (0);
 }
