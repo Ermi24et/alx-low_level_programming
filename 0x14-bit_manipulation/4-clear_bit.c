8ab9f0b81131379785c6a63f9cfd9fc8e8cc4040
Ermi24et <lilermi24@gmail.com>
2023-07-06 14:24:40 -0700

    sets bits to zero

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,5 +10,5 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	*n = *n & ~(1 << index);
-	return ((index > 8 * sizeof(*n)) ? -1: 1); 
+	return ((index > 8 * sizeof(*n)) ? -1 : 1);
 }
