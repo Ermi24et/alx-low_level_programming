9fac26d650730c316056f907661decaa95287cf9
Ermi24et <lilermi24@gmail.com>
2023-08-17 12:50:19 -0700

    adding a node at last

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp, *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (*head);
+	}
+	temp = (*head);
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new_node;
+	new_node->prev = temp;
+
+	return (*head);
+}
