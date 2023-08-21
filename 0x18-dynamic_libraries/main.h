1ef22edc8d8fd6ab1617d3d991dd088e0d18b46b
Ermi24et <lilermi24@gmail.com>
2023-08-21 14:02:07 -0700

    importing from c to pythn

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
+/*#include <stdlib.h>*/
+
+int add(int n1, int n2);
+int sub(int n1, int n2);
+int mul(int n1, int n2);
+int div(int n1, int n2);
+int mod(int n1, int n2);
 
 int _putchar(char c);
 int _islower(int c);
